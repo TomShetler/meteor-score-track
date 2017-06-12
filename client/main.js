db0d42581a4c6c5633983e7fd43d5496a5b63fa6
ThomasShetler <tshetler93@gmail.com>
2017-06-12 14:54:24 -0400

    Add mongo instance and react to client

--- client/main.js
+++ client/main.js
@@ -1,1 +1,53 @@
-console.log('From /client/main.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Players} from './../imports/api/players';
+
+Tracker.autorun(function () {
+  console.log(Players.find().fetch());
+});
+
+
+/* We have 3 ways to load:
+import 'relative path' // General files
+import 'module' // Npm modules
+import 'meteor/module' // Meteor specific modules
+*/
+const players = [{
+    _id: '1',
+    name: 'Rick',
+    score: 99
+  }, {
+    _id: '2',
+    name: 'Morty',
+    score: -1
+  }, {
+    _id: '3',
+    name: 'Birdperson',
+    score: -12
+  }];
+
+const renderPlayers = function (playersList) {
+  return playersList.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+  });
+};
+
+Meteor.startup(function () {
+  let title = 'Account Settings';
+  let name = 'Rick';
+  let jsx = (
+  <div>
+    <h1>{title}</h1>
+    <p>Hello {name}!</p>
+    <p>This is my second p</p>
+    {renderPlayers(players)}
+  </div>
+);
+  ReactDOM.render(
+    jsx,
+    document.getElementById('app')
+  );
+});
--- server/main.js
+++ server/main.js
@@ -1,1 +1,10 @@
-console.log('From /server/main.js');
+import {Players} from './../imports/api/players';
+import {Meteor} from 'meteor/meteor';
+
+Meteor.startup(function () {
+  Players.insert({
+    name: 'Morty',
+    score: 34
+  });
+  console.log(Players.find().fetch());
+});
