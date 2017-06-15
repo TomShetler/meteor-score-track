a9e2be193d91233831c214eacd4054572a4b331e
ThomasShetler <tshetler93@gmail.com>
2017-06-15 11:52:14 -0400

    Add SCSS partials to begin styling

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,12 +6,12 @@ import {Players} from './../api/players';
 export default class Player extends React.Component {
   render() {
     return (
-        <p>
-           {this.props.player.name} has {this.props.player.score} point(s).
-           <button onClick={() => Players.update(this.props.player._id, {$inc:{score: 1}})}> (+) 1</button>
-           <button onClick={() => Players.update(this.props.player._id, {$inc:{score: -1}})}> (-) 1</button>
-          <button onClick={() => Players.remove(this.props.player._id)}>Remove</button>
-        </p>
+        <div className="item">
+           <p>{this.props.player.name} has {this.props.player.score} point(s).</p>
+           <button className="button button--round" onClick={() => Players.update(this.props.player._id, {$inc:{score: 1}})}> (+) 1</button>
+           <button className="button button--round" onClick={() => Players.update(this.props.player._id, {$inc:{score: -1}})}> (-) 1</button>
+           <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>Remove</button>
+        </div>
     );
   }
 }
--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -9,9 +9,11 @@ export default class TitleBar extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        {this.renderSubTitle}
+      <div className="title-bar">
+        <div className="wrapper">
+          <h1>{this.props.title}</h1>
+          {this.renderSubTitle()}
+        </div>
       </div>
     );
   }
